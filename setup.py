4025d432ad7be53ab56358488fa9d1e14d6ecb32
bsesic <Benjamin-777@gmx.de>
2025-02-19 11:33:28 +0100

    setup.py added

---
+++ setup.py
@@ -0,0 +1,14 @@
+[build-system]
+requires = ["setuptools", "wheel"]
+
+[project]
+name = "judaicalink-docs"
+version = "0.1.0"
+dependencies = []
+
+[project.optional-dependencies]
+docs = [
+    "sphinx",
+    "sphinx-rtd-theme",
+    "sphinx-intl"
+]
