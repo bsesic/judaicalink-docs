2a01a10f7f5dcbe8a16f033a051c6d219e3b2e48
bsesic <Benjamin-777@gmx.de>
2025-02-19 11:36:33 +0100

    build added

--- docs/conf.py
+++ docs/conf.py
@@ -2,6 +2,13 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
+# Sphinx project settings
+project = 'JudaicaLink Documentation'
+author = 'JudaicaLink Team'
+version = '0.1'
+release = '0.1.0'
+
+
 # Add language support
 language = 'en'  # Default language
 locale_dirs = ['locale/']  # Path to translation files
