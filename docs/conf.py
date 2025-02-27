408617dc04d2218dab27171ea05883521aa033ea
bsesic <Benjamin-777@gmx.de>
2025-02-27 16:04:06 +0100

    German translations updated

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
-    "sphinx.ext.githubpages"
+    "sphinx.ext.githubpages",
     'sphinx.ext.todo',
     'sphinx.ext.ifconfig',
     'sphinx.ext.intersphinx',
